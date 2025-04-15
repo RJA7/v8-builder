c47e020225ec170c6a5a71b030be6ae22b3e5d35
Roman <roman.kopanskyi@softgames.de>
2025-04-15 18:51:36 +0300

    Use optimized size config for android arm8 build

--- builders/v8-android-arm-v8.sh
+++ builders/v8-android-arm-v8.sh
@@ -42,11 +42,39 @@ python ./tools/dev/v8gen.py arm64.release -vv -- '
 target_os = "android"
 target_cpu = "arm64"
 v8_target_cpu = "arm64"
-is_component_build = true
-use_custom_libcxx = false
-v8_enable_i18n_support = true
-v8_use_external_startup_data = false
 symbol_level = 1
+
+is_debug = false
+is_component_build = false
+is_official_build = true
+symbol_level = 0
+strip_debug_info = true
+use_lld = true
+use_rtti = false
+use_exceptions = false
+
+v8_monolithic = true
+v8_static_library = true
+use_custom_libcxx = false
+use_thin_archives = true
+
+v8_enable_future = false
+v8_use_external_startup_data = false
+v8_enable_i18n_support = true
+v8_enable_webassembly = false
+v8_enable_gdbjit = false
+v8_enable_disassembler = false
+v8_enable_v8_checks = false
+v8_enable_debugging_features = false
+v8_enable_test_features = false
+v8_enable_object_print = false
+v8_enable_verify_heap = false
+v8_enable_handle_zapping = false
+v8_enable_third_party_heap = false
+v8_enable_conservative_stack_scanning = false
+v8_enable_shared_ro_heap = false
+v8_deprecation_warnings = false
+v8_enable_inspector = false
 '
 ninja -C out.gn/arm64.release -t clean
 ninja -C out.gn/arm64.release v8_libplatform
